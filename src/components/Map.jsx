8001f4b91d5aff06086a2ba7f30803291e3b6c67
Jannosch Müller <janosch.mueller@betterplace.org>
2016-07-06 17:47:03 +0200

    Spread markers with same coordinates [#124453431]

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -7,6 +7,8 @@ import VolunteeringMarker from './VolunteeringMarker.jsx'
 
 var Map = React.createClass({
   render: function() {
+    this.spreadRecordsWithSameCoordinates()
+
     var markers = this.props.records.map(
       record => <VolunteeringMarker
                   highlighted={record === this.getHighlightRecord()}
@@ -111,6 +113,46 @@ var Map = React.createClass({
 
   getInfoBubbleRecord: function() {
     return this.infoBubble && this.infoBubble.isOpen() && this.infoBubble.record
+  },
+
+  spreadRecordsWithSameCoordinates: function() {
+    var recordsByLatLng = {}
+
+    for (var record of this.props.records) {
+      var key = [record.latitude, record.longitude]
+      if (!recordsByLatLng[key]) recordsByLatLng[key] = []
+      recordsByLatLng[key].push(record)
+    }
+
+    for (var key in recordsByLatLng) {
+      var recordsWithSamePosition = recordsByLatLng[key]
+      if (recordsWithSamePosition.length < 2) continue
+
+      // arrange in spiral
+
+      for (var i = 0; i < recordsWithSamePosition.length; i++) {
+        var record = recordsWithSamePosition[i]
+        var angle = 0.666 * i;
+        record.longitude += ((4 + angle) * Math.cos(angle)) / 80000;
+        record.latitude  += ((4 + angle) * Math.sin(angle)) / 120000;
+      }
+
+      // arrange in square grid
+
+      // var gridSize = Math.ceil(Math.sqrt(recordsWithSamePosition.length)),
+      //     col = 0,
+      //     row = 0
+
+      // for (var record of recordsWithSamePosition) {
+      //   record.longitude += col * 0.0002
+      //   record.latitude  -= row * 0.00012
+      //   col += 1
+      //   if (col >= gridSize) {
+      //     col = 0
+      //     row += 1
+      //   }
+      // }
+    }
   }
 })
 
