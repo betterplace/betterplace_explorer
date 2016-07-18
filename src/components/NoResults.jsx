70a9f4df7578780d8178b6303b75e3b66536d915
Roman Cieslik <post@romancieslik.de>
2016-07-18 15:25:25 +0200

    styles for zero case

--- src/components/NoResults.jsx
+++ src/components/NoResults.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 var NoResults = React.createClass({
   render: function() {
     return (
-      <div>
-        <p>Leider gibt es aktuell keine Angebote an diesem Ort.</p>
-        <p>Du kannst nachschauen, ob es in Deiner näheren Umgebung Angebote gibt, indem Du den Kartenausschnitt vergrößerst.</p>
+      <div className="text-center">
+        <h3>Leider gibt es aktuell keine Angebote an diesem Ort.</h3>
+        <p><strong>Wenn Du auf der Karte rauszoomst, kannst Du schauen, ob es in einem größeren Umkreis Ehrenämter gibt.</strong></p>
       </div>
     )
   }
