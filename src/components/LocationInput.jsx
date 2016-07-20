b094473aa78a54e20bcbfbb0ab87064ddf9bd3bf
Roman Cieslik <post@romancieslik.de>
2016-07-20 17:09:52 +0200

    adds loupe icon for search input

--- src/components/LocationInput.jsx
+++ src/components/LocationInput.jsx
@@ -13,6 +13,7 @@ var LocationInput = React.createClass({
           value={this.props.value}
           onChange={this.updateLocation}
         />
+        <i className="fa fa-search bpe--location-input--search-icon" />
         <a
           className='bpe--location-input--reset'
           onClick={this.resetInput}
